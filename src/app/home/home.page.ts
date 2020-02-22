5f4a992db60862d428eec8ac164fa5fbdb25784f
isakV123 <50157502+isakV123@users.noreply.github.com>
2020-02-21 22:25:09 -0300

    adicionado busca de pokemon por nome e numero

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DadosPokemonPage } from '../dados-pokemon/dados-pokemon.page';
 
 @Component({
   selector: 'app-home',
@@ -7,88 +8,112 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  public listaPokemons =[
+  public listaPokemons = [
 
 
-  {
-    numero: '001',
-   nome: 'Bulbasaur',
-    tipos:['Grass','Poison'],
-   img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'},
+    {
+      numero: '001',
+      nome: 'Bulbasaur',
+      tipos: ['Grass', 'Poison'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    },
 
-   {
-    numero: '002',
-    nome: 'Ivysaur',
-    tipos: ['Grass', 'Poison'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png'},
+    {
+      numero: '002',
+      nome: 'Ivysaur',
+      tipos: ['Grass', 'Poison'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png'
+    },
 
-   {
-    numero: '003',
-    nome: 'Venusaur',
-    tipos: ['Grass', 'Poison'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png'
-   },
+    {
+      numero: '003',
+      nome: 'Venusaur',
+      tipos: ['Grass', 'Poison'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png'
+    },
 
-   {
-    numero: '004',
-    nome: 'Charmander',
-    tipos: ['Fire'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
+    {
+      numero: '004',
+      nome: 'Charmander',
+      tipos: ['Fire'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'
 
-   },
+    },
 
-   {
-    numero: '005',
-    nome: 'Charmeleon',
-    tipos: ['Fire'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png'
+    {
+      numero: '005',
+      nome: 'Charmeleon',
+      tipos: ['Fire'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png'
 
-   },
+    },
 
 
-   {
-    numero: '006',
-    nome: 'Charizard',
-    tipos: ['Fire', 'Flying'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'
+    {
+      numero: '006',
+      nome: 'Charizard',
+      tipos: ['Fire', 'Flying'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'
 
-   },
+    },
 
-   {
-    numero: '007',
-    nome: 'Squirtle',
-    tipos: ['Water'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
+    {
+      numero: '007',
+      nome: 'Squirtle',
+      tipos: ['Water'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'
 
-   },
+    },
 
-   {
-    numero: '008',
-    nome: 'Wartortle',
-    tipos: ['Water'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png'
+    {
+      numero: '008',
+      nome: 'Wartortle',
+      tipos: ['Water'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png'
 
-   },
+    },
 
-   {
-    numero: '009',
-    nome: 'Blastoise',
-    tipos: ['Water'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png'
+    {
+      numero: '009',
+      nome: 'Blastoise',
+      tipos: ['Water'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png'
 
-   },
-   
-   {
-    numero: '010',
-    nome: 'Caterpie',
-    tipos: ['Bug'],
-    img:  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png'
+    },
 
-   }
+    {
+      numero: '010',
+      nome: 'Caterpie',
+      tipos: ['Bug'],
+      img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png'
+
+    }
 
   ];
-  public listaFiltrada =[];
+  public listaFiltrada = [];
 
-  constructor() {}
+  constructor() {
+    this.resetarLista();
+  }
 
+  public resetarLista() {
+    this.listaFiltrada = this.listaPokemons;
+  }
+
+  public buscarPokemon(evento: any) {
+    let busca = evento.target.value;
+    this.resetarLista();
+
+    if (busca && busca.trim() != '') {
+      this.listaFiltrada = this.listaFiltrada.filter(dados => {
+        if (dados.numero.toLowerCase().indexOf(busca.toLowerCase()) > -1) {
+          return true;
+        }
+        if (dados.nome.toLowerCase().indexOf(busca.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      })
+    }
+  }
 }
